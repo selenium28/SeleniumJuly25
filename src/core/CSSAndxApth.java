fa8a2ba50f645843c932d2898e90ecffdfee6e34
selenium28 <devselenium.class2819@gmail.com>
2025-07-11 21:04:02 +0530

    xpath

---
+++ src/core/CSSAndxApth.java
@@ -0,0 +1,38 @@
+package core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CSSAndxApth {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.manage().window().maximize();
+		
+//		xpath
+		WebElement getStarted_Button = driver.findElement(By.xpath("//button[@class='mio-button']"));
+		getStarted_Button.click();
+		
+//		CSS
+		WebElement getStarted_ButtonCSS = driver.findElement(By.xpath("button[class='mio-button']"));
+		getStarted_ButtonCSS.click();
+		
+//		text() xpath
+//		//*[text()='Admin']
+//		Index
+//		(//*[text()='Admin'])[1]
+		
+//		Following xpath
+//		//*[text()='System Users']//following::input[1]
+		
+//		Preceding
+//		(//*[text()='User Role'])[1]//preceding::input[1]
+		
+		
+		
+	}
+
+}
